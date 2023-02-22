dc1a3326441698cf5a3b25de24d395490658726b
LEEINWOO <49314154+develinu@users.noreply.github.com>
2023-02-23 00:37:29 +0900

    initial commit

---
+++ lib/bucket_service.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+/// Bucket 담당
+class BucketService extends ChangeNotifier {
+  List<Bucket> bucketList = [
+    Bucket('잠자기', false), // 더미(dummy) 데이터
+  ];
+}
